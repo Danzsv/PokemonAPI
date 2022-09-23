38a10d31f1a2b2ea15e25c5d69b6f6156cd38d82
Danzsv <danzsv94@gmail.com>
2022-09-23 18:28:38 -0500

    new try

--- api/app.js
+++ api/app.js
@@ -11,7 +11,7 @@ const PORT = process.env;
 
 app.use("/api", require("./routes"));
 
-app.listen(PORT || 5000, () => {
+app.listen(PORT, () => {
   console.log(`API Pokemon PI is ready in port ${PORT}`);
 });
 
